85af1b463cc92e19addd9953ff0f73b24e52ec3a
Carlos Apolaya <temis_it@hotmail.com>
2023-07-06 17:34:19 -0500

    Primer Entregable

---
+++ Entregable1-solucion/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import productRouter from "./routes/products.router.js"
+import cartRouter from "./routes/carts.router.js"
+const app=express();
+
+const PORT=8080;
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.Router())
+app.use("/api",productRouter)
+app.use("/api",cartRouter)
+
+
+app.listen(PORT,()=>{
+    console.log("Server is working ")
+})
---
+++ Entregable1-solucion/routes/products.router.js
@@ -0,0 +1,38 @@
+import {Router} from "express"
+import ProductManager from "../managers/productManager.js"
+import { __dirname } from "../utils.js"
+
+const manager=new ProductManager(__dirname+'/files/products.json')
+const router =Router()
+
+router.get("/products",async(req,res)=>{
+    const products= await manager.getProducts(req.query)
+    res.json({products})
+})
+
+
+
+router.get("/products/:pid", async (req, res) => {
+    const productfind = await manager.getProductbyId(req.params);
+    res.json({ status: "success", productfind });
+  });
+
+  router.post("/products", async (req, res) => {
+    const newproduct = await manager.addProduct(req.body);
+     res.json({ status: "success", newproduct });
+  });
+
+  router.put("/products/:pid", async (req, res) => {
+    const updatedproduct = await manager.updateProduct(req.params,req.body);
+     res.json({ status: "success", updatedproduct });
+  });
+
+  
+  router.delete("/products/:pid", async (req, res) => {
+    const deleteproduct = await manager.deleteProduct(req.params);
+     res.json({ status: "success",deleteproduct });
+  });
+
+
+
+export default router
