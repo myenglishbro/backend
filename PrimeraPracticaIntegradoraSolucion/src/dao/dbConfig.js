5bd8bb1b5a02dc66a9129066398c003c17f8d50f
Carlos Apolaya <temis_it@hotmail.com>
2023-07-21 00:58:05 -0500

    updated

--- PrimeraPracticaIntegradoraSolucion/src/dao/dbConfig.js
+++ PrimeraPracticaIntegradoraSolucion/src/dao/dbConfig.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const URI="mongodb+srv://carlosapolayasanchez:myenglishbro@cluster0.cbayzpv.mongodb.net/ecommerce?retryWrites=true&w=majority"
+const URI=""
 
 await mongoose.connect(URI,{
     serverSelectionTimeoutMS:5000,
